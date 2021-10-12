3e30345b603034451a275c6a2fe8c184bc4a82c7
sunnyleeyun <sunnylee945@hotmail.com>
2021-10-12 18:52:32 +0800

    Add bangla numbers

---
+++ OneStar/BanglaNumbers.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+void kuti(int d, int start) {
+	char* s[] = {"lakh", "hajar", "shata", ""};
+	int base[] = {100000, 1000, 100, 1};
+	int mod[] = {100, 100, 10, 100};
+	for (int k=0; k<4; k++) {
+		int v = d/base[k]%mod[k];
+		if (v==0 && start==false) continue;
+		start = true;
+		if (v) cout << " " << v << (k==3 ? "" : " ") << s[k];
+	}
+}
+
+int main() {
+	long long n, num=0;
+	while (cin >> n) {
+		printf("%4d", ++num); cout << ".";
+		long long base = 10000000;
+		long long a = n/base/base%base;
+		long long b = n/base%base;
+		long long c = n/1%base;
+		kuti(a, 0); if (a) cout << " kuti";
+		kuti(b, a); if (a|b) cout << " kuti";
+		kuti(c, a|b);
+		if (n==0) cout << " 0";
+		cout << endl;
+	}
+	return 0;
+}
