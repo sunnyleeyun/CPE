fbd371b62810543d7ad7edafd032f8e443401e75
sunnyleeyun <sunnylee945@hotmail.com>
2021-12-17 00:04:23 +0800

    Adjust you can say 11

--- OneStar/YouCanSay11.cpp
+++ OneStar/YouCanSay11.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-int main() {
-	string s;
-	while (cin >> s && s!="0") {
-		long long sum[2] = {0, 0};
-		for (int i=0; i<s.length(); i++) {
-			sum[i%2]+=s[i]-'0';
+int main()
+{
+	string a;
+	while (cin >> a && a != "0")
+	{
+		int even = 0;
+		int odd = 0;
+		for (int i = 0; i < a.length(); ++i)
+		{
+			if (i % 2) even += a[i] - '0';
+			else odd += a[i] - '0';
 		}
-		cout << s << " is" << ((sum[1]-sum[0])%11 ? " not ": " ") << "a multiple of 11." << endl;
+		cout << a << " is" << ((even - odd) % 11 == 0 ? " " : " not ") << "a multiple of 11." << endl;
 	}
+	return 0;
 }
