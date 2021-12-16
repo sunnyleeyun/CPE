e31f0ef9eb7827e67bf1227aa779f878a15396c9
sunnyleeyun <sunnylee945@hotmail.com>
2021-12-17 01:15:00 +0800

    Adjust list of conquests

--- OneStar/ListOfConquests.cpp
+++ OneStar/ListOfConquests.cpp
@@ -1,31 +1,24 @@
 #include <iostream>
-#include <sstream>
-#include <set>
 #include <map>
 using namespace std;
 
-int main() {
+int main()
+{
 	map<string, int> count;
-	map<string, int> ::iterator iter;
-	set<string> exist;
-	stringstream ss;
-	char entire_c[76] = {0};
-	string entire_s, first_s;
+	map<string, int>::iterator iter;
 	int n;
 	cin >> n;
 	cin.ignore();
-	while (n--) {
-		cin.getline(entire_c, 76);
-		entire_s = entire_c;
-		if (exist.count(entire_s)<=0) {
-			exist.insert(entire_s);
-			ss.str("");
-			ss << entire_s;
-			ss >> first_s;
-			count[first_s]++;
-		}
+	string country;
+	char others[76] = {};
+	while (n--)
+	{
+		cin >> country;
+		count[country]++;
+		cin.getline(others, 76);
 	}
-	for (iter = count.begin(); iter != count.end(); iter++) {
+	for (iter = count.begin(); iter != count.end(); ++iter)
+	{
 		cout << iter->first << " " << iter->second << endl;
 	}
 	return 0;
