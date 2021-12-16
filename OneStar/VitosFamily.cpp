52abd6f68bcf74d2aba0ea79ef6ab179f46cb1c5
sunnyleeyun <sunnylee945@hotmail.com>
2021-12-16 19:27:23 +0800

    Adjust Vitos Family

--- OneStar/VitosFamily.cpp
+++ OneStar/VitosFamily.cpp
@@ -1,26 +1,30 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-vector<int> num;
 
-int main() {
+int main()
+{
+	// Hint: Medium
 	int n, r, s;
 	cin >> n;
-	while (n--) {
+	vector<int> num;
+	while (n--)
+	{
 		cin >> r;
 		num.clear();
-		for (int i=0; i<r; i++) {
+		for (int i = 0; i < r; ++i)
+		{
 			cin >> s;
 			num.push_back(s);
 		}
 		sort(num.begin(), num.end());
 		int mid = num[r/2];
-		int sum = 0;
-		for (int i=0; i<r; i++) {
-			sum += abs(num[i]-mid);
+		int d = 0;
+		for (int i = 0; i < r; ++i)
+		{
+			d += abs(num[i] - mid);
 		}
-		cout << sum << endl;
+		cout << d << endl;
 	}
-	return 0;
 }
