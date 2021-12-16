30a2bb6ad4cb18d211e5ccb2f433da73518d6f6c
sunnyleeyun <sunnylee945@hotmail.com>
2021-12-16 23:28:42 +0800

    Adjust primary arithmetic

--- OneStar/PrimaryArithmetic.cpp
+++ OneStar/PrimaryArithmetic.cpp
@@ -1,35 +1,41 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
-void divide(int n, int arr[11], int &cnt) {
-	for (cnt = 0; n; cnt++) {
-		arr[cnt] = n%10;
-		n/=10;
+void divide(int n, int arr[], int &cnt)
+{
+	for (cnt = 0; n; ++cnt)
+	{
+		arr[cnt] = n % 10;
+		n /= 10;
 	}
 }
 
-int main() {
+int main()
+{
+	// Hint: Counterwiselt put the numbers in arrays
 	int a, b;
-	while (cin >> a >> b && (a != 0 || b != 0)) {
-		int c = 0;
+	while (cin >> a >> b && a != 0 && b != 0)
+	{
 		int lenA, lenB;
-		int arrA[11] = {0}, arrB[11] = {0};
-		int sum[12] = {0};
+		int arrA[11] = {}, arrB[11] = {};
+		int sum[12] = {};
 		divide(a, arrA, lenA);
 		divide(b, arrB, lenB);
-		int lenM = max(lenA, lenB);
-		for (int i = 0; i < lenM; i++) {
+		int lenMax = max(lenA, lenB);
+		int count = 0;
+		for (int i = 0; i < lenMax; ++i)
+		{
 			sum[i] += arrA[i] + arrB[i];
-			if (sum[i] >= 10) {
+			if (sum[i] >= 10)
+			{
+				count++;
+				sum[i + 1]++;
 				sum[i] -= 10;
-				sum[i+1]++;
-				c++;
 			}
 		}
-		if (c == 0) cout << "No carry operation." << endl;
-		else if (c == 1) cout << "1 carry operation." << endl;
-		else cout << c << " carry operations." << endl;
+		if (count == 0) cout << "No carry operation." << endl;
+		else if (count == 1) cout << "1 carry operation." << endl;
+		else cout << count << " carry operations." << endl;
 	}
 	return 0;
 }
