ed20d66ff14613098d7479488d3afd99174fd8c6
sunnyleeyun <sunnylee945@hotmail.com>
2021-12-18 17:22:12 +0800

    Add back to high shcool physics c++ version

---
+++ BackToHighSchoolPhysics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Hint: Some sort of formula?
+	int v, t;
+	while (cin >> v >> t)
+	{
+		cout << 2 * v * t << endl;
+	}
+	return 0;
+}
