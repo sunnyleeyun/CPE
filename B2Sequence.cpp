2eb7f2c6002eec2d6d76aba0197351b95dda979b
sunnyleeyun <sunnylee945@hotmail.com>
2021-12-18 17:15:44 +0800

    Adjust B2 Sequence

--- B2Sequence.cpp
+++ B2Sequence.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int m[10005] = {0}, n = 0, i, j, b = 1, c = 1;
-	
-	while (cin >> n) {
-		for (i = 1; i <= n; i++) {
+int main()
+{
+	// Hint: Store all -> find individual
+	int n, c = 1, m[10001] = {};
+	while (cin >> n)
+	{
+		cout << "Case #" << c << ": ";
+		int b = 1, s[20002] = {};
+		for (int i = 1; i <= n; ++i)
+		{
 			cin >> m[i];
-			if (m[i - 1] >= m[i]) b = 0;
+			if (m[i] <= m[i - 1]) b = 0;
 		}
 		
-		int note[20005] = {0};
-		if (b == 1) {
-			for (i = 1; i <= n; i++) {
-				for (j = i; j <= n; j++) {
-					if (note[m[i] + m[j]] == 1) b = 0;
-					else note[m[i] + m[j]] = 1;
+		if (b == 1)
+		{
+			for (int i = 1; i <= n; ++i)
+			{
+				for (int j = i; j <= n; ++j)
+				{
+					if (s[m[i] + m[j]] == 1) b = 0;
+					s[m[i] + m[j]] = 1;
 				}
 			}
+			
 		}
-		cout << "Case #" << c << ": It is ";
-		cout << (b ? "a B2-Sequence" : "not a B2-Sequence") << ".\n\n";
+		cout << "It is" << (b ? " " : " not ") << "a B2-Sequence.\n\n";
 		
-		c++;
+		++c;
 	}
 	return 0;
 }
