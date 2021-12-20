eb2be841da18158a5f3f88a91338e5628f48dd47
sunnyleeyun <sunnylee945@hotmail.com>
2021-12-20 20:48:53 +0800

    Adjust cheapest base

--- CheapestBase.cpp
+++ CheapestBase.cpp
@@ -1,37 +1,37 @@
 #include <iostream>
 using namespace std;
-const int MAX_L = 36;
 
-int main() {
-	int n; cin >> n;
-	for (int k = 1; k <= n; k++) {
-		if (k > 1) cout << endl;
-		cout << "Case " << k << ":\n";
-		int a[MAX_L];
-		for (int i = 0; i < MAX_L; i++) {
-			cin >> a[i];
-		}
-		int nn; cin >> nn;
-		while (nn--) {
-			int v; cin >> v;
-			int b[MAX_L + 1];
-			for (int i = 2; i <= MAX_L; i++) {
-				b[i] = 0;
-				for (int j = v; j; j/=i) {
-					b[i] += a[j % i];
-				}
+int main()
+{
+	// Hint: Create matrix -> calculate each and find min -> output min
+	int c;
+	cin >> c;
+	for (int i = 1; i <= c; ++i)
+	{
+		if (i > 1) cout << endl;
+		cout << "Case " << i << ":" << endl;
+		int m[36];
+		for (int j = 0; j < 36; ++j) cin >> m[j];
+		int n;
+		cin >> n;
+		while (n--)
+		{
+			int x;
+			cin >> x;
+			cout << "Cheapest base(s) for number " << x << ":";
+			
+			int minInk = 2147483647, ink[37] = {};
+			for (int i = 2; i <= 36; ++i)
+			{
+				for (int v = x; v; v /= i) ink[i] += m[v % i];
+				if (ink[i] < minInk) minInk = ink[i];
 			}
-			cout << "Cheapest base(s) for number " << v << ":";
-			int mi = b[2];
-			for (int i = 3; i <= MAX_L; i++) {
-				if (b[i] < mi) mi = b[i];
-			}
-			for (int i = 2; i <= MAX_L; i++) {
-				if (b[i] == mi) cout << " " << i;
+			for (int i = 2; i <= 36; ++i)
+			{
+				if (ink[i] == minInk) cout << " " << i;
 			}
 			cout << endl;
 		}
 	}
-	
 	return 0;
 }
