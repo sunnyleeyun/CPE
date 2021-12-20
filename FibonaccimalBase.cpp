508db7b7ad83f93bc77808caa0d4442af0565ceb
sunnyleeyun <sunnylee945@hotmail.com>
2021-12-20 19:27:20 +0800

    Adjust Fibonaccimal Base

--- FibonaccimalBase.cpp
+++ FibonaccimalBase.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
+	// Hint: Create the Fib first. -> reverse and print out
 	int f[40] = {0, 1};
-	for (int i = 2; i < 40; i++) {
+	for (int i = 2; i < 40; ++i)
+	{
 		f[i] = f[i - 1] + f[i - 2];
 	}
+
 	int n;
 	cin >> n;
-	while (n--) {
+	while (n--)
+	{
 		int m;
 		cin >> m;
 		cout << m << " = ";
-		bool preone = false;
-		for (int i = 39; i >= 2; i--) {
-			if (m >= f[i]) {
+		bool isBegin = false;
+		for (int j = 39; j >= 2; --j)
+		{
+			if (m >= f[j])
+			{
+				m -= f[j];
+				isBegin = true;
 				cout << "1";
-				m -= f[i];
-				preone = true;
 			}
-			else if (preone == true) {
-				cout << 0;
-			}
+			else if (isBegin) cout << "0";
 		}
-		cout << " (fib)\n";
+		cout << " (fib)" << endl;
 	}
-		
 	return 0;
 }
