2eb009ca68faee52dcd038bf914438e5d76aaa86
sunnyleeyun <sunnylee945@hotmail.com>
2021-12-20 21:48:35 +0800

    Adjust divide but not quite conquier

--- DivideButNotQuiteConquer.cpp
+++ DivideButNotQuiteConquer.cpp
@@ -1,27 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int a, b; 
-	while (cin >> a >> b) {
-		if (a < 2 || b < 2) {
+int main()
+{
+	// Hint: It's ok to repeat
+	int m, n;
+	while (cin >> m >> n)
+	{
+		if (m < 2 || n < 2)
+		{
 			cout << "Boring!" << endl;
 			continue;
 		}
-		int x = a;
-		while (x % b == 0 && x > 1) {
-			x /= b;
-		}
-		if (x == 1) {
-			while (a % b == 0 && a > 1) {
-				cout << a << " ";
-				a /= b;
-			}
-			cout << "1" << endl;
-		} else {
-			cout << "Boring!" << endl;
+		int t = m;
+		while (t % n == 0 && t > 1)
+		{
+			t /= n;
 		}
 		
+		if (t != 1) cout << "Boring!" << endl;
+		else
+		{
+			while (m % n == 0 && m > 1)
+			{
+				cout << m << " ";
+				m /= n;
+			}
+			cout << "1" << endl;
+		}
 	}
 	return 0;
 }
