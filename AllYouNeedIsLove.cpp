ca1cf82730a5bebddde49bdb8e7a8b3b0c71cb02
sunnyleeyun <sunnylee945@hotmail.com>
2021-12-20 21:37:27 +0800

    Adjust all you need is love

--- AllYouNeedIsLove.cpp
+++ AllYouNeedIsLove.cpp
@@ -1,37 +1,39 @@
 #include <iostream>
 using namespace std;
-char S1[31], S2[31];
+char s[31], l[31];
 
-int stringToInt(char s[31]) {
+int stringToInt(char str[31])
+{
 	int res = 0;
-	for (int i = 0; i < 30; i++) {
-		if (s[i] == '\0') break;
+	for (int i = 0; i < 31; ++i)
+	{
+		if (str[i] == '\0') break;
 		res *= 2;
-		if (s[i] == '1') res++;
+		if (str[i] == '1') res++;
 	}
 	return res;
 }
 
-int gcd(int p, int q) {
-	if (p < q) return gcd(q, p);
+int gcd(int p, int q)
+{
+	if (q > p) return gcd(q, p);
 	if (q == 0) return p;
 	return gcd(q, p % q);
 }
 
-int main() {
-	int n; cin >> n;
-	for (int j = 1; j <= n; j++) {
-		cin >> S1 >> S2;
-		int n1, n2;
-		n1 = stringToInt(S1);
-		n2 = stringToInt(S2);
-		cout << "Pair #" << j << ": ";
-		if (gcd(n1, n2) > 1) {
-			cout << "All you need is love!\n";
-		}
-		else {
-			cout << "Love is not all you need!\n";
-		}
+int main()
+{
+	// Hint: gcd
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; ++i)
+	{
+		cout << "Pair #" << i << ": ";
+		cin >> s >> l;
+		int S = stringToInt(s);
+		int L = stringToInt(l);
+		if (gcd(S, L) > 1) cout << "All you need is love!" << endl;
+		else cout << "Love is not all you need!" << endl;
 	}
 	return 0;
 }
