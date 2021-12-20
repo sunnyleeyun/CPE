f573511ecf3343ae1d0a4a3fd24d1bebe5da5126
sunnyleeyun <sunnylee945@hotmail.com>
2021-12-20 19:45:42 +0800

    Adjust Funny Encription Method

--- FunnyEncrpytionMethod.cpp
+++ FunnyEncrpytionMethod.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int n; cin >> n;
-	while (n--) {
-		int m; cin >> m;
+int main()
+{
+	// Hint: Only need to add the remainder
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		int m;
+		cin >> m;
 		int b1 = 0, b2 = 0;
 		for (int v = m; v; v/=2) b1 += v % 2;
-		for ( ; m; m/=10) {
+		while (m)
+		{
 			for (int v = m % 10; v; v/=2) b2 += v % 2;
+			m /= 10;
 		}
-		cout << b1 << " " << b2 << "\n";
+		cout << b1 << " " << b2 << endl;
 	}
 	return 0;
 }
